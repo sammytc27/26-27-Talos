79c3d508ab23ce9dc5ea0d3d356d2130c08c84fc
sammytc27 <tlachi-coyotl27s@ncssm.edu>
2026-09-23 17:34:20 -0400

    test motor changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 // Chassis constructor
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {1, 2, 3},     // Left Chassis Ports (negative port will reverse it!)
+    {1, 2, 13},     // Left Chassis Ports (negative port will reverse it!)
     {-4, -5, -6},  // Right Chassis Ports (negative port will reverse it!)
 
     7,      // IMU Port
