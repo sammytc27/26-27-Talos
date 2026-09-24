e793b173b674d947a099a278f46f0f7db7333c19
sammytc27 <tlachi-coyotl27s@ncssm.edu>
2026-09-24 11:49:34 -0400

    changed wheel diameter

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@
 // Chassis constructor
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {1, 2, 13},     // Left Chassis Ports (negative port will reverse it!)
+    {1, 2, 3},     // Left Chassis Ports (negative port will reverse it!)
     {-4, -5, -6},  // Right Chassis Ports (negative port will reverse it!)
 
     7,      // IMU Port
-    4.125,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
+    2.75,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
     343);   // Wheel RPM = cartridge * (motor gear / wheel gear)
 
 // Uncomment the trackers you're using here!
