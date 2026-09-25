37a0b0643a0befb263de20e696e5f388e8b8de1d
sammytc27 <tlachi-coyotl27s@ncssm.edu>
2026-09-24 20:29:10 -0400

    motor changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 // Chassis constructor
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {1, 2, 3},     // Left Chassis Ports (negative port will reverse it!)
-    {-4, -5, -6},  // Right Chassis Ports (negative port will reverse it!)
+    {1, 2},     // Left Chassis Ports (negative port will reverse it!)
+    {-4, -5},  // Right Chassis Ports (negative port will reverse it!)
 
     7,      // IMU Port
     2.75,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
